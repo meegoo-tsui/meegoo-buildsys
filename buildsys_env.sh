77f6763ea786bc9f3e6c0c3ea5f99aaccb55ffe6
meegoo tsui <meegoo.tsui@gmail.com>
2012-07-05 00:13:33 +0800

    python printf.

--- buildsys_env.sh
+++ buildsys_env.sh
@@ -8,22 +8,20 @@
 #+   meegoo.tsui@gmail.com, 2012-06-28
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [ "$BUILD_SYS_ENV" == "" ]; then
-	# Set flag
 	export BUILD_SYS_ENV=OK
-
-	./shell/inc/print_color.sh -y "[Set buildsys path]:"
 	export BUILD_SYS_PATH=$PWD
-	./shell/inc/print_color.sh -g "BUILD_SYS_PATH = $BUILD_SYS_PATH"
+	$BUILD_SYS_PATH/shell/inc/print_color.sh -y "[Set buildsys path]:"
+	$BUILD_SYS_PATH/shell/inc/print_color.sh -g "BUILD_SYS_PATH = $BUILD_SYS_PATH"
 	
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	./shell/inc/print_color.sh -y "[Add User tools to path]:"
 	export PATH="$BUILD_SYS_PATH/shell/bin:$PATH"
 	export PATH="$BUILD_SYS_PATH/python/bin:$PATH"
-	./shell/inc/print_color.sh -g "PATH = $PATH"
+	$BUILD_SYS_PATH/shell/inc/print_color.sh -y "[Add User tools to path]:"
+	$BUILD_SYS_PATH/shell/inc/print_color.sh -g "PATH = $PATH"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 else
-	./shell/inc/print_color.sh -y "config env is done."
+	$BUILD_SYS_PATH/shell/inc/print_color.sh -y "config env is done."
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- python/inc/utils.py
+++ python/inc/utils.py
@@ -1,31 +1,81 @@
-## @package pyexample
-#  Documentation for this module.
-#
-#  More details.
+#! /usr/bin/python
 
-## Documentation for a function.
-#
-#  More details.
-def func():
-    pass
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## @file    utils.py
+#  @brief   utils package for all.
+#  @author  meegoo.tsui@gmail.com
+#  @date    2012/07/04
 
-## Documentation for a class.
-#
-#  More details.
-class PyClass:
-   
-    ## The constructor.
-    def __init__(self):
-        self._memVar = 0;
-   
-    ## Documentation for a method.
-    #  @param self The object pointer.
-    def PyMethod(self):
-        pass
-     
-    ## A class variable.
-    classVar = 0;
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## @package utils
+#  @brief   utils package for all.
+#  @author  meegoo.tsui@gmail.com
+#  @date    2012/07/04
 
-    ## @var _memVar
-    #  a member variable
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import os, sys
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## color reset
+txtrst = "\033[1;m"
+## color red
+txtred = "\033[1;31m"
+## color green
+txtgrn = "\033[1;32m"
+## color yellow
+txtylw = "\033[1;33m"
+## color blue
+txtblu = "\033[1;34m"
+## color purple
+txtpur = "\033[1;35m"
+## color cyan
+txtcyn = "\033[1;36m"
+## color white
+txtwht = "\033[1;37m"
+## color list
+color_list = [txtrst, txtred, txtgrn, txtylw, txtblu, txtpur, txtcyn, txtwht]
+## Printf message with colors.
+class printf:
+	## Printf message.
+	#  @param color int
+	#  @param message string
+	def printf(self, color, message):
+		if color > 7 or color < 0:
+			color = 0
+		print color_list[color] + message + color_list[0]
+
+	## printf test info.
+	def test(self):
+		self.printf(0, "\tcolor 0 - message...")
+		self.printf(1, "\tcolor 1 - message...")
+		self.printf(2, "\tcolor 2 - message...")
+		self.printf(3, "\tcolor 3 - message...")
+		self.printf(4, "\tcolor 4 - message...")
+		self.printf(5, "\tcolor 5 - message...")
+		self.printf(6, "\tcolor 6 - message...")
+		self.printf(7, "\tcolor 7 - message...")
+
+	## printf error info then exit.
+	def error(self, message):
+		self.printf(1, "<error>\n\t" + message)
+		sys.exit(1)
+
+	## printf status info.
+	def status(self, message):
+		self.printf(4, "<status>\n\t" + message)
+
+## object of class printf
+printf = printf()
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Test all packages.
+def main():
+	printf.status("Test class printf.")
+	printf.test()
+	printf.status("Test class printf ok.")
+
+	sys.exit(0)
+
+if __name__ == '__main__':
+	main()
 
