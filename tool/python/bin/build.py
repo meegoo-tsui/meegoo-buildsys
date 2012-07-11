c4037cedc766e71a54f04f80289c43a97149fcb2
meegoo tsui <meegoo.tsui@gmail.com>
2012-07-11 13:09:20 +0800

    Project for pydev of eclipse.

--- tool/python/bin/build.py
+++ tool/python/bin/build.py
@@ -14,7 +14,7 @@ import os, sys
 ## buildsys import path
 buildsys_import_path = os.environ["BUILD_SYS_PATH"] + "/tool/python"
 if buildsys_import_path not in sys.path:
-     sys.path.insert(0, buildsys_import_path)
+    sys.path.insert(0, buildsys_import_path)
 import buildsys_import
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
